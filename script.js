865d9606694e92132e4e8f223e4faa876ddc2bbe
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-01 21:33:44 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement('h2');
+h2.textContent = "This content added by JavaScript";
+document.querySelector('body').appendChild(h2);
+
+const h3 = document.createElement('h2');
+h3.textContent = "This was a coffee break";
+document.querySelector('body').appendChild(h3);
